fb29ec850f65132a746575b6e8b8effaf32b849e
kanghuijin <rkd8527@naver.com>
2026-01-11 21:10:20 +0900

    refactor: 리드미 수정

--- src/main/java/com/example/remittanceservice/presentation/dto/TransactionDto.java
+++ src/main/java/com/example/remittanceservice/presentation/dto/TransactionDto.java
@@ -1,6 +1,5 @@
 package com.example.remittanceservice.presentation.dto;
 
-import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Positive;
